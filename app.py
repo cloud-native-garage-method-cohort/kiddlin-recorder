a0f150f80444a4fd52d4c2b5fae0ebe2b8d4be4a
linkaidi <linkaidi@cn.ibm.com>
2021-06-17 10:16:35 +0800

    fix bug

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ import json
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI', default='mysql+pymysql://root:123456@localhost:3306/wordpress-mysql')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', default=True)
-db = SQLAlchemy(app, use_native_unicode='utf8')
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI', default='mysql+pymysql://root:123456@localhost:3306/wordpress-mysql')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', default=True)
+# db = SQLAlchemy(app, use_native_unicode='utf8')
 
 
-class Member(db.Model):
-    __tablename__ = 'squad5_members'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    role = db.Column(db.String(255))
-    reserved = db.Column(db.String(255))
+# class Member(db.Model):
+#     __tablename__ = 'squad5_members'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(255))
+#     role = db.Column(db.String(255))
+#     reserved = db.Column(db.String(255))
 
 
 @app.route('/', methods=['GET'])
@@ -22,22 +22,22 @@ def index():
     return 'Squad 5 API'
 
 
-@app.route('/search', methods=['POST'])
-def search():
-    data = json.loads(json.dumps(request.form, ensure_ascii=False))
-    print(data)
-    member = Member.query.filter(Member.name==data['name']).first()
-    return jsonify({'name': member.name, 'role': member.role, 'reserved': member.reserved})
-
-
-@app.route('/add', methods=['POST'])
-def add():
-    data = json.loads(json.dumps(request.form, ensure_ascii=False))
-    member = Member(name=data['name'], role=data['role'], reserved=['reserved'])
-    db.session.add(member)
-    db.session.commit()
-    return jsonify({'result': "OK"})
+# @app.route('/search', methods=['POST'])
+# def search():
+#     data = json.loads(json.dumps(request.form, ensure_ascii=False))
+#     print(data)
+#     member = Member.query.filter(Member.name==data['name']).first()
+#     return jsonify({'name': member.name, 'role': member.role, 'reserved': member.reserved})
+#
+#
+# @app.route('/add', methods=['POST'])
+# def add():
+#     data = json.loads(json.dumps(request.form, ensure_ascii=False))
+#     member = Member(name=data['name'], role=data['role'], reserved=['reserved'])
+#     db.session.add(member)
+#     db.session.commit()
+#     return jsonify({'result': "OK"})
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0')
